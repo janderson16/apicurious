69d5d2960b1cea36337b4b200fcff91eb0d6cab8
James Anderson <james.p.anderson16@gmail.com>
2017-03-22 17:08:52 -0600

    styling for dashboard, added navbar to app.html

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session.clear
-
     redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#show'
   get  '/dashboard', to: 'dashboard#show'
   get '/auth/github/callback', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   # get '/followers', to: 'followers#index'
   # get '/following', to: 'following#index'
   # get '/repos', to: 'repos#index'
